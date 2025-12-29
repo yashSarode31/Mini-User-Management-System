85d4c1fc8b15f086932f2eab33b76378047253c4
yashSarode31 <114423228+yashSarode31@users.noreply.github.com>
2025-12-30 00:13:36 +0530

    Password hashing with bcrypt

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema(
   {
@@ -36,6 +37,17 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 )
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
